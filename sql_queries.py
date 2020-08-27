37d407b3dec77299af9b6da677ad88202db45b8f
Ben Selleslagh <ben@dataframe.be>
2020-08-27 16:05:38 +0200

    minor bug fix

--- sql_queries.py
+++ sql_queries.py
@@ -39,7 +39,7 @@ user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level)
 VALUES (%s, %s, %s, %s, %s) 
 ON CONFLICT (user_id) DO UPDATE
-SET level = users.level
+SET level = EXCLUDED.level
 """)
 
 song_table_insert = ("""
